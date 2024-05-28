9bb61faa4fcc01601415b6492725d2e41f98d106
im-Ayesha <161641742+im-Ayesha@users.noreply.github.com>
2024-05-28 18:00:23 +0500

    assignment

---
+++ 6,7,8 stripping names,Number Eight, Create lines/app.js
@@ -0,0 +1,10 @@
+//Stripping Name
+var personName = "\n\t BABAR AZAM\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
+//Number Eight
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
---
+++ 6,7,8 stripping names,Number Eight, Create lines/app.ts
@@ -0,0 +1,11 @@
+//Stripping Name
+let personName: string =`\n\t BABAR AZAM\t\n`;
+console.log(personName);
+let stripped: string = personName.trim();
+console.log(stripped);
+
+//Number Eight
+console.log(5+3);
+console.log(11-3);
+console.log(4*2);
+console.log(16/2);
