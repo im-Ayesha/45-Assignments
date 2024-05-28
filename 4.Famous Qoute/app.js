a236a2dd51cd6903a9bc47ab38d916e8db5b0cd5
im-Ayesha <161641742+im-Ayesha@users.noreply.github.com>
2024-05-28 17:17:04 +0500

    Assignment

---
+++ 4.Famous Qoute/app.js
@@ -0,0 +1,3 @@
+var qoute = "A person who never made a miistake never tried anything new";
+var author = "Albrt Einstein";
+console.log("".concat(author, "once said,\"").concat(qoute));
---
+++ 4.Famous Qoute/app.ts
@@ -0,0 +1,3 @@
+let qoute: string ="A person who never made a miistake never tried anything new";
+let author: string ="Albrt Einstein"
+console.log(`${author}once said,"${qoute}`);
