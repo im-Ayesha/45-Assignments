248da5f40f6c2ba0d9f6d3f53e0103cfa541cc1b
im-Ayesha <161641742+im-Ayesha@users.noreply.github.com>
2024-05-28 18:03:13 +0500

    assignment

---
+++ 11,12 Name,Greetings/app.js
@@ -0,0 +1,11 @@
+//Name
+//let members: string[]=['azam','zaid','aira','sima','siera'];
+//for(let i=0; i<members.length; i++){
+// console.log(members[i]);
+//}
+//Greetings
+var members = ['wahaj', 'meerab', 'zarnab', 'azlan', 'saby', 'tabi'];
+var message = 'kal kahein jana hy kia:';
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ 11,12 Name,Greetings/app.ts
@@ -0,0 +1,12 @@
+//Name
+//let members: string[]=['azam','zaid','aira','sima','siera'];
+//for(let i=0; i<members.length; i++){
+   // console.log(members[i]);
+//}
+
+//Greetings
+let members: string[]=['wahaj','meerab','zarnab','azlan','saby','tabi'];
+let message: string='kal kahein jana hy kia:';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}    
